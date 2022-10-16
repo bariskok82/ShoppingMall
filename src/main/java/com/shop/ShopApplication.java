3e6cdaa8b03932cdd5556749ab4b5426a2c80480
bariskok82 <96614664+bariskok82@users.noreply.github.com>
2022-10-16 20:59:35 +0900

    Add files via upload

---
+++ src/main/java/com/shop/ShopApplication.java
@@ -0,0 +1,22 @@
+package com.shop;
+
+import com.shop.dto.MemberFormDto;
+import com.shop.entity.Member;
+import com.shop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@RequiredArgsConstructor
+@SpringBootApplication
+public class ShopApplication {
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopApplication.class, args);
+    }
+
+}
